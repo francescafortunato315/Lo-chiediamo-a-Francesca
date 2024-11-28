aff49a2dcfabe1f0656e4ad22e46c13bdb4b2de1
francescafortunato315 <164329079+francescafortunato315@users.noreply.github.com>
2024-11-28 15:17:07 +0100

    Update Francesca_app2.py

--- Francesca_app2.py
+++ Francesca_app2.py
@@ -129,6 +129,7 @@ elif st.session_state["authentication_status"]:
         "   - Stile preferito.\n"
         "   - Altri dettagli specificati nel profilo o nella richiesta del cliente.\n\n"
         "2. Presenta ogni prodotto trovato con il **nome proprio** (esempio Maglione Leonardo, Abito Sofia,...) e fornisci una descrizione dettagliata basata sulle sue caratteristiche, che devono includere:\n"
+        "   - **Nome proprio.**\n"
         "   - **Taglie disponibili.**\n"
         "   - **Genere.**\n"
         "   - **Occasione d'uso.**\n"
